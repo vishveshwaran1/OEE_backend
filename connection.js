950f1cd42afee558f069c5970a2a4e3eb79fdb4f
SparkOEE <sparkinvotech001@gmail.com>
2025-02-01 16:29:43 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -4,8 +4,7 @@ const mongoose = require("mongoose");
 const connection = async () => {
     try {
       //await mongoose.connect('mongodb+srv://yazhini:yazhini@cluster0.xrihspk.mongodb.net/noble_evergreen');
-      //await mongoose.connect('mongodb+srv://demo:demo@cluster0.8xdzq.mongodb.net/testing_oee');
-        await mongoose.connect('mongodb://localhost:27017/testing_oee');
+      await mongoose.connect('mongodb+srv://demo:demo@cluster0.8xdzq.mongodb.net/testing_oee');
       console.log('Connected to MongoDB');
     } catch (e) {
       console.log('Connection error:', e);
