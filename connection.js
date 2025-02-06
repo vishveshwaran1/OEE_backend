a033872f964652700ff22477d06213a0feba2f53
Ragul D <raguld2105@gmail.com>
2025-02-06 07:47:31 +0530

    time checking

--- connection.js
+++ connection.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 
 const connection = async () => {
     try {
-      //await mongoose.connect('mongodb+srv://yazhini:yazhini@cluster0.xrihspk.mongodb.net/noble_evergreen');
-      //await mongoose.connect('mongodb+srv://demo:demo@cluster0.8xdzq.mongodb.net/testing_oee');
-      await mongoose.connect('mongodb+srv://demo:demo@cluster0.zfvaz.mongodb.net/testing_oee');
+      
+      await mongoose.connect('mongodb+srv://demo:demo@cluster0.8xdzq.mongodb.net/testing_oee');
+      //await mongoose.connect('mongodb+srv://demo:demo@cluster0.zfvaz.mongodb.net/testing_oee');
       console.log('Connected to MongoDB');
     } catch (e) {
       console.log('Connection error:', e);
