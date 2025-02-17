3fbf1fa6d2b204228536443fc2588b48ab47b4de
Ragul D <raguld2105@gmail.com>
2025-02-17 08:01:45 +0530

    new date time added, running time chart rectified

--- connection.js
+++ connection.js
@@ -4,8 +4,9 @@ const mongoose = require("mongoose");
 const connection = async () => {
     try {
       
-      await mongoose.connect('mongodb+srv://demo:demo@cluster0.8xdzq.mongodb.net/testing_oee');
+      //await mongoose.connect('mongodb+srv://demo:demo@cluster0.8xdzq.mongodb.net/testing_oee');
       //await mongoose.connect('mongodb+srv://demo:demo@cluster0.zfvaz.mongodb.net/testing_oee');
+      await mongoose.connect('mongodb+srv://demo:demo@cluster0.8xdzq.mongodb.net/transform');
       console.log('Connected to MongoDB');
     } catch (e) {
       console.log('Connection error:', e);
