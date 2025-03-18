bf24c0a2b9d25cf6eb3f81c1369abc6c90c37e6f
yazhini_gayathri <yazhinigayathri.2002@gmail.com>
2025-03-18 11:41:20 +0530

    rectified running chart problem1

--- connection.js
+++ connection.js
@@ -6,8 +6,8 @@ const connection = async () => {
       
       //await mongoose.connect('mongodb+srv://demo:demo@cluster0.8xdzq.mongodb.net/testing_oee');
       //await mongoose.connect('mongodb+srv://demo:demo@cluster0.zfvaz.mongodb.net/testing_oee');
-      //await mongoose.connect('mongodb+srv://demo:demo@cluster0.8xdzq.mongodb.net/transform');
-      await mongoose.connect('mongodb+srv://demo:demo@cluster0.8xdzq.mongodb.net/transform_oee');
+      await mongoose.connect('mongodb+srv://demo:demo@cluster0.8xdzq.mongodb.net/transform');
+      //await mongoose.connect('mongodb+srv://demo:demo@cluster0.8xdzq.mongodb.net/transform_oee');
       console.log('Connected to MongoDB');
     } catch (e) {
       console.log('Connection error:', e);
