f8b8ab95687eb3a91b090913659a6ac169e96824
yazhini_gayathri <yazhinigayathri.2002@gmail.com>
2025-04-27 22:59:18 +0530

    new Mongodb conxn

--- connection.js
+++ connection.js
@@ -6,8 +6,9 @@ const connection = async () => {
       
       //await mongoose.connect('mongodb+srv://demo:demo@cluster0.8xdzq.mongodb.net/testing_oee');
       //await mongoose.connect('mongodb+srv://demo:demo@cluster0.zfvaz.mongodb.net/testing_oee');
-      await mongoose.connect('mongodb+srv://demo:demo@cluster0.8xdzq.mongodb.net/transform');
+      //await mongoose.connect('mongodb+srv://demo:demo@cluster0.8xdzq.mongodb.net/transform');
       //await mongoose.connect('mongodb+srv://demo:demo@cluster0.8xdzq.mongodb.net/transform_oee');
+      await mongoose.connect('mongodb+srv://admin:admin@cluster0.h8veorm.mongodb.net/oee');
       console.log('Connected to MongoDB');
     } catch (e) {
       console.log('Connection error:', e);
